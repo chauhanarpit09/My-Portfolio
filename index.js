c675236b47023e076b8f4337e3718e203b3a0b76
Arpit Chauhan <arpit09chauhan@gmail.com>
2020-08-21 23:40:24 +0530

    my portfolio

---
+++ index.js
@@ -0,0 +1,15 @@
+function openSM(){
+    document.getElementById('mySidemenu').style.width = "220px";
+    document.getElementById('pg-content').style.marginLeft = "220px";
+}
+function closeSM(){
+    document.getElementById('mySidemenu').style.width = "0";
+    document.getElementById('pg-content').style.marginLeft = "0";
+}
+
+function openAB(){
+    document.getElementById('rightAB').style.float = "right";
+    document.getElementById('rightAB').style.width = "50%";
+   
+    
+}
