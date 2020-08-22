ad8e5a728706b38537fc255ff8a9ff8002435c96
Arpit Chauhan <arpit09chauhan@gmail.com>
2020-08-23 03:54:35 +0530

    updated on 23

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function openSM(){
     document.getElementById('sheading').innerHTML = "";
     document.getElementById('mySidemenu').style.width = "220px";
     document.getElementById('mySidemenu').style.height = screen.height + "10";
-    document.getElementById('pg-content').style.marginLeft = "220px";
+    
 }
 function closeSM(){
     document.getElementById('mySidemenu').style.width = "0";
@@ -10,44 +10,45 @@ function closeSM(){
     document.getElementById('sheading').innerHTML = "&nbsp;&#9776;";
 }
 
-function openAB(){
-    document.getElementById('rightAB').style.float = "right";
-    document.getElementById('rightAB').style.width = "50%";
-   
-    
+
+var x = window.matchMedia("(max-width: 500px)")
+if(screen.width>500){
+
 }
-function myFunction(x) {
-    if (x.matches) { // If media query matches
-      document.getElementById('svg').innerHTML = " "
-      document.getElementById('name').innerHTML = " "
-      var i = document.getElementById('imageother').innerHTML
-      document.getElementById('imageother').innerHTML = document.getElementById('listother').innerHTML
-      document.getElementById('listother').innerHTML = i
+else if(x.matches){
+    myFunction()
+}
+else{
+    myFunction()
+}
+console.log(x)
+function myFunction() {
 
-      document.getElementById('skillsvg').innerHTML = " ";
-      var topskill = document.getElementById('TopSkills');
-      topskill.style.background = "#90ee90";
+        document.getElementById('svg').style.width = "0"
+        document.getElementById('name').innerHTML= " "
+        var i = document.getElementById('imageother').innerHTML
+        document.getElementById('imageother').innerHTML = document.getElementById('listother').innerHTML
+        document.getElementById('listother').innerHTML = i
 
-      var inte = document.getElementById('in')
-      inte.style.marginTop = "50px";
-     
-    }
+        document.getElementById('skillsvg').innerHTML = " ";
+        var topskill = document.getElementById('TopSkills');
+        topskill.style.background = "#90ee90";
+
+        var inte = document.getElementById('in')
+        inte.style.marginTop = "50px";
+    
   }
   
-  var x = window.matchMedia("(max-width: 700px)")
-  myFunction(x)
-  x.addListener(myFunction) 
 
-  var interes = ["Machine Learning","Web Development","Data Science","DevOps","App Development"] 
-  var c = ["#fee715ff","#55AB55","#FFA500","white","#ff3333"]
-  var h2 = document.getElementById('interest')
-  var j=0;
-  i = setInterval(function(){
-            if(j==interes.length){
-                j=0;
-            }
-            h2.style.color = c[j]
-            h2.innerHTML = interes[j]
-            
-            j = j+1
-  },1000)
+var interes = ["Machine Learning","Web Development","Data Science","DevOps","App Development"] 
+var c = ["#fee715ff","#55AB55","#FFA500","white","#ff3333"]
+var h2 = document.getElementById('interest')
+var j=0;
+i = setInterval(function(){
+    if(j==interes.length){
+         j=0;
+     }
+    h2.style.color = c[j]
+    h2.innerHTML = interes[j]            
+    j = j+1
+},1000)
